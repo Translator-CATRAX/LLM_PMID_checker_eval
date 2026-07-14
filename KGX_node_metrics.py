39c7b2e672b707fda3bb19250f33ac8148c24d07
Sandrine Muller <sandrine.muller.research@gmail.com>
2026-07-14 17:45:31 +0200

    Update KGX_node_metrics.py
    externalizing file reading from computation

--- KGX_node_metrics.py
+++ KGX_node_metrics.py
@@ -1,5 +1,6 @@
 import json
 from collections import Counter
+from tqdm import tqdm
 
 
 
@@ -7,28 +8,37 @@ from collections import Counter
 ## Compute metrics:
 
 
-def nodes_degree(input_KGX_file):
+def nodes_degree(kgx_dict):
     degree_counter = Counter()
-    with open(input_KGX_file, encoding='utf-8') as f:
-        for line in f:
-            try:
-                data = json.loads(line)
-                # Update count for both nodes in the edge
-                degree_counter[data['subject']] += 1
-                degree_counter[data['object']] += 1
-            except (json.JSONDecodeError, KeyError):
-                continue
+    for edge in kgx_dict:
+        try:
+            degree_counter[kgx_dict[edge]['subject']] += 1
+            degree_counter[kgx_dict[edge]['object']] += 1
+        except (json.JSONDecodeError, KeyError):
+            continue
 
     # Save all nodes and their degrees to a JSON file
     all_degrees = dict(degree_counter)
     return all_degrees
 
-def get_biolink_category_mapping(id_list,mapping_file):
-    return []
+def get_biolink_category_mapping(id_list,category_mapping):
 
-def compute_KGX_node_metrics(input_KGX_file):
-    node_metrics = dict()
-    node_metrics["degree"] = nodes_degree(input_KGX_file)
+    node_category = dict()
+            
+    for id in id_list:
+        if id in category_mapping.keys():
+            node_category[id] = category_mapping[id]
+        else:
+            print(f'could not find id:{id} in mapping file')
+
+    return node_category
+
+def compute_KGX_node_metrics(kgx_dict,category_mapping_dict):
+    node_degree = nodes_degree(kgx_dict)
+    node_category = get_biolink_category_mapping(list(node_degree.keys()),category_mapping_dict)
+
+    print('bob')
+    return node_degree, node_category
 # os.makedirs(os.path.dirname(output_file), exist_ok=True)
 # with open(output_file, 'w', encoding='utf-8') as f:
 #     json.dump(all_degrees, f, indent=4)
