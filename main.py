8e8298a387ad17b8573d43140bf8086f671985db
Sandrine Muller <sandrine.muller.research@gmail.com>
2026-07-15 22:11:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,38 @@ import os
 import KGX_node_metrics
 import json
 from tqdm import tqdm
-import bmt
 import sys
+import re
 
-def get_highest_parent_below_namedthing(toolkit, cat_name):
-    """
-    Calcule le parent le plus élevé qui n'est pas NamedThing et la profondeur.
-    Utilise toolkit.get_parent de manière itérative.
-    """
-    current_node = cat_name
-    depth = 0
-    while True:
-        parent = toolkit.get_parent(current_node, False)
-        # Si pas de parent trouvé ou si le parent est NamedThing, on s'arrête
-        if not parent or "NamedThing" in parent:
-            return current_node, depth
-        else:
-            current_node = parent
-            depth += 1
+def extract_mixin_values(input_string):
+    # Pattern explanation:
+    # %s      -> matches literal '%s'
+    # to      -> matches literal 'to'
+    # mixin   -> matches literal 'mixin'
+    pattern = r"(.*?)to(.*?)mixin"
+    
+    match = re.search(pattern, input_string)
+    
+    if match:
+        # Extract the captured groups and convert to integers (or keep as strings)
+        s1 = match.group(1)
+        s2 = match.group(2)
+        return s1, s2
+    else:
+        return None, None
 
-def read_KGX(input_KGX_file,headers_of_interest = ['subject','object','predicate','category','publications']):
+
+def read_KGX(input_KGX_file,headers_of_interest = ['subject','object','predicate','publications']):
     # need to verify that data['id'] is unique
     kgx_dict = dict()
      
     
     with open(input_KGX_file, encoding='utf-8') as f:
         total_lines = sum(1 for _ in f)
-    
+        print(f'Total lines:{total_lines}')
+    cpt = 1
     with open(input_KGX_file, encoding='utf-8') as f:
-        for line in tqdm(f, total=total_lines, desc="Processing mapping"):
+        for line in tqdm(f, total=total_lines, desc="Import KGX"):
             try:
                 data = json.loads(line)
                 if 'id' in data.keys():
@@ -45,23 +48,28 @@ def read_KGX(input_KGX_file,headers_of_interest = ['subject','object','predicate
                             additional_data[k] = data[k]
                     kgx_dict[data['id']]['additional_data'] = additional_data
             except (json.JSONDecodeError, KeyError):
+                print(f'Read line {cpt} issue')
                 continue
+            cpt += 1
 
     return kgx_dict
 
-def read_mapping(mapping_file):
+
+
+
+def read_KGX_category_mapping(mapping_file):
+
     category_mapping = dict()
-    toolkit = bmt.toolkit.Toolkit()
-    # Cache pour éviter de recalculer la hiérarchie pour chaque ligne
-    # Structure: { category_name: (biolink_branch, depth) }
-    cache_branch = {}
 
     # line counter:
     with open(mapping_file, encoding='utf-8') as f:
         total_lines = sum(1 for _ in f)
+        print(f'Total lines:{total_lines}')
+    
+    cpt = 1
   
     with open(mapping_file, encoding='utf-8') as f:
-        for line in tqdm(f, total=total_lines, desc="Processing mapping"):
+        for line in tqdm(f, total=total_lines, desc="Processing category mapping and biolink model metrics"):
             try:
                 data = json.loads(line)
                 id = data['id']
@@ -71,31 +79,62 @@ def read_mapping(mapping_file):
                         cat_name = data['category'][0]
                         category_mapping[id]['category'] = cat_name
                         category_mapping[id]['name'] = data['name']
-                        
-                        
-                        branch, depth = get_highest_parent_below_namedthing(toolkit, cat_name)
-                        category_mapping[id]['biolink_branch'] = branch
-                        category_mapping[id]['depth'] = depth
 
             except (json.JSONDecodeError, KeyError):
+                print(f'Read line {cpt} issue')
                 continue
+        cpt += 1
     return category_mapping
 
 def main(kgx_dict,category_mapping_dict):
     # Compute metrics:
-    KGX_nodes_metrics = KGX_node_metrics.compute_KGX_node_metrics(kgx_dict,category_mapping_dict)
+    KGX_nodes_metrics,biolink_info = KGX_node_metrics.compute_KGX_node_metrics(kgx_dict,category_mapping_dict)
 
-    print('bob')
+    KGX_metrics = []
+    total_lines = len(kgx_dict.keys())
+    for edge_id in tqdm(kgx_dict.keys(), total=total_lines, desc="Mapping metrics"):
+        subject = kgx_dict[edge_id]['subject']
+        object = kgx_dict[edge_id]['object']
+        predicate = kgx_dict[edge_id]['predicate']
+
+        KGX_edge_metrics = {}
+        # append subject metrics:
+        KGX_edge_metrics['id'] = edge_id
+        KGX_edge_metrics['subject'] = subject
+        KGX_edge_metrics['subject_degree'] = KGX_nodes_metrics[subject]['degree']
+        KGX_edge_metrics['subject_biolink_category'] = KGX_nodes_metrics[subject]['biolink_category']
+        KGX_edge_metrics['subject_biolink_branch'] = KGX_nodes_metrics[subject]['biolink_branch']
+        KGX_edge_metrics['subject_biolink_depth'] = KGX_nodes_metrics[subject]['biolink_depth']
+        KGX_edge_metrics['subject_name'] = KGX_nodes_metrics[subject]['name']
+
+        # append object metrics:
+        KGX_edge_metrics['object'] = object
+        KGX_edge_metrics['object_degree'] = KGX_nodes_metrics[object]['degree']
+        KGX_edge_metrics['object_biolink_category'] = KGX_nodes_metrics[object]['biolink_category']
+        KGX_edge_metrics['object_biolink_branch'] = KGX_nodes_metrics[object]['biolink_branch']
+        KGX_edge_metrics['object_biolink_depth'] = KGX_nodes_metrics[object]['biolink_depth']
+        KGX_edge_metrics['object_name'] = KGX_nodes_metrics[object]['name']
+
+        # append predicate metrics:
+        KGX_edge_metrics['predicate'] = predicate
+        KGX_edge_metrics['predicate_biolink_branch'] = biolink_info[predicate]['biolink_branch']
+        KGX_edge_metrics['predicate_biolink_depth'] = biolink_info[predicate]['biolink_depth']
+        KGX_edge_metrics['publications_number'] = len(kgx_dict[edge_id])
+
+        KGX_metrics.append(KGX_edge_metrics)
+
+
+    return KGX_metrics
 
 
 if __name__ == "__main__":
     # load data (TO BE UPDATED AFTER AUTOMATION):
-    input_KGX_file = './data/kg2.10.3_semmeddb_for_dogpark_uncapped_2026_04_07/transform_892b6acb/normalization_2025sep1/normalized_edges.jsonl'
-    biolink_id_to_category_mapping = './data/kg2.10.3_semmeddb_for_dogpark_uncapped_2026_04_07/transform_892b6acb/normalization_2025sep1/merged_nodes.jsonl'
-    output_file = './data/KGX_computed_degrees.json'
+    input_KGX_file = 'data/kg2.10.3_semmeddb_dogpark_uncapped_2026_04_07/transform_892b6acb/normalization_2025sep1/normalized_edges.jsonl'
+    biolink_id_to_category_mapping = 'data/kg2.10.3_semmeddb_dogpark_uncapped_2026_04_07/transform_892b6acb/normalization_2025sep1/merged_nodes.jsonl'
+    output_file = 'data/KGX_computed_edges_metrics.json'
 
     # Vérification de l'existence des fichiers avant de commencer
-    required_files = [input_KGX_file, biolink_id_to_category_mapping]
+    required_files = [os.path.abspath(input_KGX_file), os.path.abspath(biolink_id_to_category_mapping)]
     missing_files = [f for f in required_files if not os.path.exists(f)]
 
     if missing_files:
@@ -104,9 +143,11 @@ if __name__ == "__main__":
             print(f"  - {f}")
         sys.exit(1)
 
-    print('load KGX:')
     kgx_dict = read_KGX(input_KGX_file)
-    print('load node category IDs:')
-    category_mapping_dict = read_mapping(biolink_id_to_category_mapping)
+    category_mapping_dict = read_KGX_category_mapping(biolink_id_to_category_mapping)
 
-    main(kgx_dict,category_mapping_dict)
+    KGX_edge_metrics = main(kgx_dict,category_mapping_dict)
+
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump(KGX_edge_metrics, f, indent=4)
